8e9e0dad25939843bacd44b51489461a2383f865
Martin Grønbekk Moen <martin@mennt.no>
2023-09-23 03:03:53 +0200

    Changes

--- pages/kommunestyret.vue
+++ pages/kommunestyret.vue
@@ -5,7 +5,6 @@
             <p class="mt-2 text-sm text-gray-700">Oversikt over alle medlemmer i kommunestyret, med partitilhørighet.</p>
         </div>
         <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-            <UButton label="Nytt kommunestyre" @click="openModalNewCouncil" />
             <UModal v-model="modalNewCouncilIsOpen">
                 <UCard :ui="{ divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
                     <template #header>
@@ -47,8 +46,37 @@
                     </UForm>
                     <template #footer>
                         <template class="flex justify-between">
-                            <UButton icon="i-heroicons-trash" size="sm" color="red" variant="outline" label="Slett" :trailing="false" />
+                            <UButton icon="i-heroicons-trash" size="sm" color="red" variant="outline" label="Slett" :trailing="false" @click="deleteCouncil(editedCouncil.id)" />
                             <UButton icon="i-heroicons-pencil-square" size="sm" color="primary" variant="solid" label="Lagre" :trailing="false" @click="submitFormEditCouncil" />
+                        </template>
+                    </template>
+                </UCard>
+            </UModal>
+            <UModal v-model="modalNewCouncilMemberIsOpen" :ui="{ base: 'overflow-visible' }">
+                <UCard :ui="{ divide: 'divide-y divide-gray-100 dark:divide-gray-800', base: 'overflow-visible' }">
+                    <template #header>
+                        <div class="flex items-center justify-between">
+                            <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">Nytt kommunestyremedlem</h3>
+                            <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="modalNewCouncilMemberIsOpen = false" />
+                        </div>
+                    </template>
+                    <UAlert v-show="errorMessage" :description="errorMessage" class="mb-3" color="red" variant="subtle" />
+                    <UForm ref="formNewCouncilMember" :state="newCouncilMember" @submit="addCouncilMember">
+                        <UFormGroup label="Politiker" name="politician" class="mb-3">
+                            <USelectMenu v-model="selectedPolitician" :options="politicians" :searchable="searchPoliticians" searchablePlaceholder="Søk etter politiker">
+                                <template #label>
+                                    <span v-if="selectedPolitician">{{ selectedPolitician.name }} ({{ selectedPolitician.Parties.name }})</span>
+                                    <span v-else>Velg politiker</span>
+                                </template>
+                                <template #option="{ option: politician }">
+                                    <span>{{ politician.name }} ({{ politician.Parties.name }})</span>
+                                </template>
+                            </USelectMenu>
+                        </UFormGroup>
+                    </UForm>
+                    <template #footer>
+                        <template class="flex flex-row-reverse">
+                            <UButton icon="i-heroicons-pencil-square" size="sm" color="primary" variant="solid" label="Lagre" :trailing="false" @click="submitFormNewCouncilMember" />
                         </template>
                     </template>
                 </UCard>
@@ -58,12 +86,16 @@
     <div class="mt-8 flow-root">
         <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                <UButton label="Nytt medlem" @click="openModalNewCouncilMember" />
                 <div class="flex flex-row-reverse mb-2">
-                    <div v-if="selectedCouncil" class="basis-1/7 ml-2">
+                    <div class="ml-2">
+                        <UButton label="Nytt kommunestyre" @click="openModalNewCouncil" />
+                    </div>
+                    <div v-if="selectedCouncil" class="ml-2">
                         <UButton label="Detaljer" @click="openModalViewCouncil" block />
                     </div>
                     <div class="basis-1/4">
-                        <USelectMenu v-model="selectedCouncil" :options="councils" :searchable="search" creatable>
+                        <USelectMenu v-model="selectedCouncil" :options="councils">
                             <template #label>
                                 <span v-if="selectedCouncil">{{ format(new Date(selectedCouncil.from)) }} - {{ format(new Date(selectedCouncil.to)) }}</span>
                                 <span v-else>Velg kommunestyre</span>
@@ -78,30 +110,15 @@
                     <table class="min-w-full divide-y divide-gray-300">
                         <thead class="bg-white">
                             <tr>
-                                <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">Navn</th>
-                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Title</th>
-                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">E-post</th>
-                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Rolle</th>
-                                <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-3">
-                                    <span class="sr-only">Edit</span>
-                                </th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Politiker</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Parti</th>
                             </tr>
                         </thead>
                         <tbody class="bg-white">
-                            <template v-for="location in locations" :key="location.name">
-                                <tr class="border-t border-gray-200">
-                                    <th colspan="5" scope="colgroup" class="bg-gray-50 py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">{{ location.name }}</th>
-                                </tr>
-                                <tr v-for="(person, personIdx) in location.people" :key="person.email" :class="[personIdx === 0 ? 'border-gray-300' : 'border-gray-200', 'border-t']">
-                                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">{{ person.name }}</td>
-                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.title }}</td>
-                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.email }}</td>
-                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ person.role }}</td>
-                                    <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
-                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, {{ person.name }}</span></a>
-                                    </td>
-                                </tr>
-                            </template>
+                            <tr v-for="(councilMember, councilMemberIdx) in councilMembers" :key="councilMember.id" :class="[councilMemberIdx === 0 ? 'border-gray-300' : 'border-gray-200', 'border-t']">
+                                <td class="whitespace-nowrap px-3 py-4 text-sm">{{ councilMember.Politicians.name }}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm">{{ councilMember.Politicians.Parties.name }}</td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
@@ -116,7 +133,10 @@ import '@vuepic/vue-datepicker/dist/main.css'
 
 const supabase = useSupabaseClient()
 const councils = ref([])
+const councilMembers = ref([])
+const politicians = ref([])
 const selectedCouncil = ref(undefined)
+const selectedPolitician = ref(undefined)
 const errorMessage = ref(undefined)
 
 const format = (date) => {
@@ -130,6 +150,13 @@ const search = async (q) => {
     if (!q) return councils.value
     return councils.value.filter((council) => {
         return format(new Date(council.from)).includes(q) || format(new Date(council.to)).includes(q)
+    })
+}
+
+const searchPoliticians = async (q) => {
+    if (!q) return politicians.value
+    return politicians.value.filter((politician) => {
+        return politician.name.toLowerCase().includes(q.toLowerCase()) || politician.Parties.name.toLowerCase().includes(q.toLowerCase())
     })
 }
 
@@ -152,6 +179,7 @@ const submitFormEditCouncil = async () => {
 
 const modalNewCouncilIsOpen = ref(false)
 const modalViewCouncilIsOpen = ref(false)
+const modalNewCouncilMemberIsOpen = ref(false)
 
 const newCouncil = ref({
     from: new Date(),
@@ -162,6 +190,11 @@ const editedCouncil = ref({
     id: undefined,
     from: new Date(),
     to: new Date(new Date().setFullYear(new Date().getFullYear() + 4))
+})
+
+const newCouncilMember = ref({
+    politician: undefined,
+    council: undefined
 })
 
 function openModalNewCouncil() {
@@ -181,6 +214,15 @@ function openModalViewCouncil() {
     modalViewCouncilIsOpen.value = true
 }
 
+function openModalNewCouncilMember() {
+    selectedPolitician.value = undefined
+    if (!politicians.value || politicians.value.length == 0) getPoliticians()
+    newCouncilMember.value = {
+        politician: undefined,
+        council: undefined
+    }
+    modalNewCouncilMemberIsOpen.value = true
+}
 
 async function addCouncil() {
     const { data, error } = await supabase
@@ -197,6 +239,7 @@ async function addCouncil() {
         errorMessage.value = error.message
     }
     else {
+        selectedCouncil.value = data[0]
         getCouncils()
         modalNewCouncilIsOpen.value = false
     }
@@ -221,6 +264,22 @@ async function editCouncil() {
     }
 }
 
+async function deleteCouncil(id) {
+    const { error } = await supabase
+        .from('Councils')
+        .delete()
+        .eq('id', id)
+
+    if (error) {
+        errorMessage.value = error.message
+    }
+    else {
+        selectedCouncil.value = undefined
+        getCouncils()
+        modalViewCouncilIsOpen.value = false
+    }
+}
+
 async function getCouncils() {
     let { data, error } = await supabase
         .from('Councils')
@@ -228,7 +287,35 @@ async function getCouncils() {
         .order('from')
 
     councils.value = data
+
+    if (selectedCouncil.value) selectedCouncil.value = data.find((council) => council.id === selectedCouncil.value.id)
+    else selectedCouncil.value = data[0]
 }
+
+async function getPoliticians() {
+    let { data } = await supabase
+        .from('Politicians')
+        .select('id,name,Parties (id, name)')
+        .order('name')
+
+    politicians.value = data
+}
+
+async function getCouncilMembers() {
+    let { data } = await supabase
+        .from('CouncilMembers')
+        .select(`
+            id,
+            Politicians (id, name, Parties (id, name))
+        `)
+        .eq('council', selectedCouncil.value.id)
+
+    councilMembers.value = data
+}
+
+watch(selectedCouncil, async (newCouncil, oldCouncil) => {
+    if (newCouncil && newCouncil.id !== oldCouncil?.id) getCouncilMembers()
+})
 
 onMounted(() => {
     getCouncils()

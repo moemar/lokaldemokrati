af8c98850973639fc22d9b51283f858100677b2d
Martin Grønbekk Moen <martin@mennt.no>
2023-10-22 01:37:19 +0200

    Changes

--- pages/council-meetings/[id].vue
+++ pages/council-meetings/[id].vue
@@ -8,12 +8,28 @@
     <div class="mt-8 flow-root">
         <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                OK
+                <pre>{{ councilMeeting }}</pre>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
+const route = useRoute()
+const supabase = useSupabaseClient()
+const councilMeeting = ref(undefined)
 
+async function getCouncilMeeting() {
+    let { data, error } = await supabase
+        .from('CouncilMeetings')
+        .select()
+        .eq('id', route.params.id)
+
+    if (error) councilMeeting.value = undefined
+    else councilMeeting.value = data[0]
+}
+
+onMounted(() => {
+    getCouncilMeeting()
+})
 </script>
